681f3d8adefb991e136f7e0a5d3face548ffe2d1
kgthompson1111 <kgthompson@null.net>
2021-11-21 16:33:02 -0700

    created calculator gui - no js functionality yet

---
+++ script.js
@@ -0,0 +1,21 @@
+/* addition, subtraction, multiplication and division functions */
+
+function add(x, y) {
+    return (x + y);
+}
+
+function subtract(x, y) {
+    return (x - y);
+}
+
+function multiply(x, y) {
+    return (x * y);
+}
+
+function divide(x, y) {
+    return (x / y);
+}
+
+function operate(x, y) {
+    return add(x, y);
+}
